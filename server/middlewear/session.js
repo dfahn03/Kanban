5b6bcc0b4ed0855ffedb4643b168060d1196bfd2
Dallin Fahnestock <dfahn03@gmail.com>
2019-08-15 13:06:10 -0600

    deploying

--- server/middlewear/session.js
+++ server/middlewear/session.js
@@ -2,7 +2,7 @@ import expressSession from 'express-session'
 var mongoStore = require("connect-mongodb-session")(expressSession);
 
 var store = new mongoStore({
-    uri: "mongodb://student:student333@kanban-shard-00-00-mmkqw.azure.mongodb.net:27017,kanban-shard-00-01-mmkqw.azure.mongodb.net:27017,kanban-shard-00-02-mmkqw.azure.mongodb.net:27017/kanban?ssl=true&replicaSet=Kanban-shard-0&authSource=admin&retryWrites=true&w=majority",
+    uri: "mongodb://student:student333@kanban-shard-00-00-mmkqw.azure.mongodb.net:27017,kanban-shard-00-01-mmkqw.azure.mongodb.net:27017,kanban-shard-00-02-mmkqw.azure.mongodb.net:27017/Kanban?ssl=true&replicaSet=Kanban-shard-0&authSource=admin&retryWrites=true&w=majority",
     collection: "Sessions"
 });
 
